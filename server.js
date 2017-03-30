d5e67dd5f109fd7c18a0bc9c7145b82dc6be0932
Marcel Rösler <roesler1@gmx.de>
2017-03-30 17:22:46 +0200

    ssl support

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ var moment = require('moment');
 var parseString = require('xml2js').parseString;
 var server = express();
 
-server.get('/hello', function (req, res) {
-    console.log(req)
-    console.log(res)
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('/etc/apache2/ssl-keys/newkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/apache2/ssl-keys/cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+server.get('/', function (req, res) {
+    console.log("hello")
+    res.send("hello");
 })
 
 server.get('/mugrom', function (req, res) {
@@ -18,10 +24,15 @@ server.get('/mugrom', function (req, res) {
             res.send(result["helden"]["held"][0]);
             console.log("/mugrom "+moment().format('H:mm:ss')+" Uhr am "+moment().format('DD.MM.YYYY'))
         });
-
     });
 });
 
-server.listen(3000, function () {
-    console.log('Server listening on port 3000!');
+var httpServer = http.createServer(server);
+var httpsServer = https.createServer(credentials, server);
+
+httpServer.listen(3000, function () {
+    console.log('httpServer listening on port 3000!');
 });
+httpsServer.listen(8083, function (){
+    console.log('httpsServer listening on port 8083!');
+})
